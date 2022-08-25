1959a68721b6431db229a17c4f00ecbc3bf98b98
Linden Hutchinson <hutchinson.linden@gmail.com>
2022-08-26 01:27:06 +1000

    feat: allow seeding of child models
    implement function to allow the seeding of subsequent child models after a parent model has been seeded

--- dream-big-api/app/api/entities/star_system_entity.rb
+++ dream-big-api/app/api/entities/star_system_entity.rb
@@ -1,6 +1,8 @@
 module Entities
-    class StarSystemEntity < Grape::Entity
-      expose :id
-      expose :status
-    end
+  class StarSystemEntity < Grape::Entity
+    expose :id
+    expose :status
+    expose :name
+    expose :student_journey_id
   end
+end
