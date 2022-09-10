2018273f6d4ba779cafee79d39a6372a8c9eed01
maddernd <dreambig@DESKTOP-ECMP9JD.localdomain>
2022-09-10 12:36:29 +1000

    feature/backendAuthJWT

--- dream-big-api/config/routes.rb
+++ dream-big-api/config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+# root "articles#index"
 mount DreamBigApi => '/'
+resources :users, param: :_username
+post '/auth/login', to: 'authentication#login' 
+get '/*a', to: 'application#not_found'
 
 mount GrapeSwaggerRails::Engine => '/api/docs'
 end
