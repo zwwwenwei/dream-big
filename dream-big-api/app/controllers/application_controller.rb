5529bc267e53e71075d69326f6b1feb2efd8458c
maddernd <maddernd@deakin.edu.au>
2022-09-14 11:48:57 +1000

    feature:backendtidyup

--- dream-big-api/app/controllers/application_controller.rb
+++ dream-big-api/app/controllers/application_controller.rb
@@ -1,18 +1,13 @@
-class ApplicationController < ActionController::Base
-    def not_found
-        render json: { error: 'not_found' }
-      end
-    
-      def authorize_request
-        header = request.headers['Authorization']
-        header = header.split(' ').last if header
-        begin
-          @decoded = JsonWebToken.decode(header)
-          @current_user = User.find(@decoded[:user_id])
-        rescue ActiveRecord::RecordNotFound => e
-          render json: { errors: e.message }, status: :unauthorized
-        rescue JWT::DecodeError => e
-          render json: { errors: e.message }, status: :unauthorized
-        end
+class ApplicationController < ActionController::API
+  include JsonWebToken
+
+  before_action :authenticate_request
+
+  private
+      def authenticate_request
+        header = request.headers["Authorization"]
+        header = header.split(" ").last if header
+        decoded = jwt_decode(header)
+        @current_user = User.find(decoded[:user_id])
       end
 end
--- dream-big-api/app/models/user.rb
+++ dream-big-api/app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
+    require "securerandom"
     has_secure_password
     validates :email, presence: true, uniqueness: true
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
     validates :username, presence: true, uniqueness: true
-    validates :password,
-              length: { minimum: 6 },
-              if: -> { new_record? || !password.nil? }
+    validates :password, presence: true
     
     #Associations
     has_one :student
