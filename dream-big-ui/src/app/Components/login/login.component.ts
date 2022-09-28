b40bc9359b98b4a42fc4f920ab3e2ffd0e4045c6
maddernd <maddernd@deakin.edu.au>
2022-09-28 15:34:22 +1000

    feature:fixLoginCurrnetUser

--- dream-big-ui/src/app/Components/login/login.component.ts
+++ dream-big-ui/src/app/Components/login/login.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
- 
+import { first } from 'rxjs/operators';
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
   email = '';
   password = '';
   isLoading = false;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error = '';
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    // redirect to home if already logged in
+    if (this.authService.currentUserValue) { 
+      this.router.navigate(['/profile']);
+    }
      
   }
   Login() {
@@ -23,15 +39,35 @@ export class LoginComponent implements OnInit {
    
   }
  
-  ngOnInit() { }
+  ngOnInit()  {
+    this.loginForm = this.fb.group({
+        username: ['', Validators.required],
+        password: ['', Validators.required]
+    });
 
-//This is a temp solution and does not actually auth the user
-  onSubmit() {
-    this.isLoading = true;
-    this.authService.login(this.email, this.password)
-    }
-      
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
 }
 
+// convenience getter for easy access to form fields
+get f() { return this.loginForm.controls; }
+
+
+//This is a temp solution and does not actually auth the user
+onSubmit() {
+  this.submitted = true;
+  this.loading = true;
+  this.authService.login(this.email, this.password)
+      .pipe(first())
+      .subscribe(
+          data => {
+              this.router.navigate(['/intro']);
+          },
+          error => {
+              this.error = error;
+              this.loading = false;
+          });
+}
+}
 
 
