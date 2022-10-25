fcde96b053ea449f44ecdf16a790a9bfe3b9e3d9
Linden Hutchinson <hutchinson.linden@gmail.com>
2022-10-25 14:24:57 +1100

    fix: docker now migrates DB and runs UI when created

---
+++ db-init/create_databases.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS dream_big_api_development;
+CREATE DATABASE IF NOT EXISTS dream_big_api_test;
+
+GRANT ALL ON *.* TO 'dream'@'%';
--- dream-big-api/db/migrate/20221009235730_change_journey_star.rb
+++ dream-big-api/db/migrate/20221009235730_change_journey_star.rb
@@ -1,5 +1,5 @@
 class ChangeJourneyStar < ActiveRecord::Migration[7.0]
   def change
-    add_foreign_key :journey_Stars, :student_journeys, on_delete: :cascade, column: :student_journey_id
+    add_foreign_key :journey_stars, :student_journeys, on_delete: :cascade, column: :student_journey_id
   end
 end
