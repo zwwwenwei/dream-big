1685a5f051e30a01352102f6c8b96c6e6242e341
Linden Hutchinson <hutchinson.linden@gmail.com>
2022-11-19 14:41:16 +1100

    remove unnecessary model tests

--- dream-big-api/test/models/incomplete/journey_star_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class JourneyStarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- dream-big-api/test/models/incomplete/weight_value_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class WeightValueTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
