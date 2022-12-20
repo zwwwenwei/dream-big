ec95f7895da4c2fd73cc70de0af916a280d9695e
zwwwenwei <102543390+zwwwenwei@users.noreply.github.com>
2022-12-20 16:52:22 +1100

    Figma Implementation

--- dream-big-ui/src/app/Components/SolarSystem/section-dialog.component.ts
+++ dream-big-ui/src/app/Components/SolarSystem/section-dialog.component.ts
@@ -1,5 +1,6 @@
+import { style } from "@angular/animations";
 import { DialogRef, DIALOG_DATA } from "@angular/cdk/dialog";
-import { Component, Inject } from "@angular/core";
+import { Component, Inject, Input } from "@angular/core";
 import { Section } from "src/app/helpers/types";
 
 export interface DialogData {
@@ -10,6 +11,59 @@ export interface DialogData {
 	selector: 'app-section-dialog',
 	templateUrl: 'section-dialog.component.html',
 })
+
+
+
+
+
 export class SectionDialogComponent {
+
+
 	constructor(public dialogRef: DialogRef<string>, @Inject(DIALOG_DATA) public data: Section) { }
+
+  color1 = "option";
+  color2 = "option";
+  color3 = "option";
+  color4 = "option";
+  color5 = "option";
+  color6 = "option";
+  color7 = "option";
+  color8 = "option";
+  color9 = "option";
+  updatecolor1()
+  {
+      this.color1 = "selected";
+  }
+  updatecolor2()
+  {
+      this.color2 = "selected";
+  }
+  updatecolor3()
+  {
+      this.color3 = "selected";
+  }
+  updatecolor4()
+  {
+      this.color4 = "selected";
+  }
+  updatecolor5()
+  {
+      this.color5 = "selected";
+  }
+  updatecolor6()
+  {
+      this.color6 = "selected";
+  }
+  updatecolor7()
+  {
+      this.color7 = "selected";
+  }
+  updatecolor8()
+  {
+      this.color8 = "selected";
+  }
+  updatecolor9()
+  {
+      this.color9 = "selected";
+  }
 }
