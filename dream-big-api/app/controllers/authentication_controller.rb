be0f71c053030c40fd162a1d203c17f4229d77c3
zwwwenwei <102543390+zwwwenwei@users.noreply.github.com>
2023-05-24 02:45:51 +1000

    authentication_controller.rb
    With a nested class called Authentication, I've made a new module called LoginLibrary. The SSO login as well as the standard login logic are both contained in the Authentication class.
    
    Your AuthenticationController can use the library's methods rather than implementing the login logic yourself once the login system has been moved to the library.

--- dream-big-api/app/controllers/authentication_controller.rb
+++ dream-big-api/app/controllers/authentication_controller.rb
@@ -1,29 +1,49 @@
-class AuthenticationController < ApplicationController
-    skip_before_action :authenticate_request
-  
-    # POST /auth/login
-    def login
-      @user = User.find_by_email(params[:email])
-      if @user&.authenticate(params[:password])
-        token = jwt_encode(user_id: @user.id)
-        render json: { token: token }, status: :ok
+module LoginLibrary
+  class Authentication
+    def self.login(email, password)
+      user = User.find_by_email(email)
+      if user&.authenticate(password)
+        token = jwt_encode(user_id: user.id)
+        { token: token }
       else
-        render json: { error: 'unauthorized' }, status: :unauthorized
+        { error: 'unauthorized' }
       end
     end
-  
-    # POST /auth/sso_login
-    def sso_login
-      sso_token = params[:sso_token]
+
+    def self.sso_login(sso_token)
       user_info = SSO.verify(sso_token)
-  
-      @user = User.find_by_email(user_info[:email])
-      unless @user
-        
-        @user = User.create(email: user_info[:email], username: user_info[:username])
+      user = User.find_by_email(user_info[:email])
+      unless user
+        user = User.create(email: user_info[:email], username: user_info[:username])
       end
-  
-      token = jwt_encode(user_id: @user.id)
-      render json: { token: token }, status: :ok
+
+      token = jwt_encode(user_id: user.id)
+      { token: token }
     end
   end
+end
+
+
+class AuthenticationController < ApplicationController
+  skip_before_action :authenticate_request
+
+  # POST /auth/login
+  def login
+    result = LoginLibrary::Authentication.login(params[:email], params[:password])
+    if result[:token]
+      render json: { token: result[:token] }, status: :ok
+    else
+      render json: { error: result[:error] }, status: :unauthorized
+    end
+  end
+
+  # POST /auth/sso_login
+  def sso_login
+    result = LoginLibrary::Authentication.sso_login(params[:sso_token])
+    if result[:token]
+      render json: { token: result[:token] }, status: :ok
+    else
+      render json: { error: result[:error] }, status: :unauthorized
+    end
+  end
+end
