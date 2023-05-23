4b79955089dc4689448f4a0a76b596158db1f88b
zwwwenwei <102543390+zwwwenwei@users.noreply.github.com>
2023-05-24 02:48:17 +1000

    users_controller.rb
    Installed and present in your project's Gemfile is the login_library gem. Depending on how your login library is implemented, modify the code as necessary.

--- dream-big-api/app/controllers/users_controller.rb
+++ dream-big-api/app/controllers/users_controller.rb
@@ -1,62 +1,66 @@
+require 'login_library'
+
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show, :destroy]
-  
-    # GET /users
-    def index
-      @users = User.all
-      render json: @users, status: :ok
-    end
-  
-    # GET /users/{username}
-    def show
-      render json: @user, status: :ok
-    end
-  
-    # POST /users
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        render json: @user, status: :created
-      else
-        render json: { errors: @user.errors.full_messages },
-               status: :unprocessable_entity
-      end
-    end
-  
-    # PUT /users/{username}
-    def update
-      unless @user.update(user_params)
-        render json: { errors: @user.errors.full_messages },
-               status: :unprocessable_entity
-      end
-    end
-  
-    def find_user
-      @user = User.find_by_username!(params[:_username])
-      rescue ActiveRecord::RecordNotFound
-        render json: { errors: 'User not found' }, status: :not_found
-    end
-  
-    # DELETE /users/{username}
-    def destroy
-      @user.destroy
-    end
-  
-    private
-      def user_params
-        params.permit(:username, :email, :password)
-      end
-  
-      def set_user
-        @user = User.find(params[:id])
-      end
-  
-      def authenticate_sso_request
-        token = request.headers['Authorization']
-        @user = User.find_by_sso_token(token)
-  
-        unless @user
-          render json: { error: 'unauthorized' }, status: :unauthorized
-        end
-      end
+  before_action :set_user, only: [:show, :destroy]
+  before_action :authenticate_sso_request, except: [:create]
+
+  # GET /users
+  def index
+    @users = User.all
+    render json: @users, status: :ok
   end
+
+  # GET /users/{username}
+  def show
+    render json: @user, status: :ok
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  # PUT /users/{username}
+  def update
+    unless @user.update(user_params)
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
+  def find_user
+    @user = User.find_by_username!(params[:_username])
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'User not found' }, status: :not_found
+  end
+
+  # DELETE /users/{username}
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def authenticate_sso_request
+    token = request.headers['Authorization']
+    result = LoginLibrary::Authentication.login_with_sso(token)
+
+    if result[:error]
+      render json: { error: 'unauthorized' }, status: :unauthorized
+    end
+  end
+end
